6d3dd3d27973cdb2ad75f23752d622db53367f3e
KuzarinM <kuzarinm@mail.ru>
2026-04-06 17:58:35 +0300

    Добавлена работа с CAPTCHA на базовом уровне. Нужно ещй прокинуть извменения и проверить

--- SecretDrop/Pages/Index.cshtml.cs
+++ SecretDrop/Pages/Index.cshtml.cs
@@ -1,7 +1,10 @@
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Options;
+using SecretDrop.Configurations;
 using SecretDrop.Models;
+using SecretDrop.Services.Implementations;
 using SecretDrop.Services.Interfaces;
 
 namespace SecretDrop.Pages
@@ -12,14 +15,29 @@ namespace SecretDrop.Pages
     public class IndexModel : PageModel
     {
         private readonly ISecretFileStore _store;
-        public IndexModel(ISecretFileStore store) => _store = store;
+        private readonly AppOptions _appOptions;
+        private readonly ICaptchaService _captchaService;
+        public IndexModel(ISecretFileStore store, IOptions<AppOptions> appOptions, ICaptchaService captchaService)
+        {
+            _store = store;
+            _appOptions = appOptions.Value;
+            _captchaService = captchaService;
+        }
+
+        public string PublickKey =>_appOptions.CaptchaSiteKey;
+        public bool IsCaptchaEnabled => _appOptions.UseCaptcha;
 
         public void OnGet() { }
 
         // 1. Старт сессии загрузки
-        public IActionResult OnPostInit([FromBody] InitRequest req)
+        public async Task<IActionResult> OnPostInit([FromBody] InitRequest req)
         {
-            // ВАЖНО: Передаем req.PasswordSalt и req.WrappedKey в сервис!
+            // Валидация капчи
+            if (!await _captchaService.VerifyToken(req.CaptchaToken ?? ""))
+            {
+                return BadRequest("Captcha validation failed.");
+            }
+
             var id = _store.InitUpload(req.Meta, req.Ttl, req.Limit, req.PasswordSalt, req.WrappedKey);
             return new JsonResult(new { id });
         }
